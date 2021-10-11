f4710396fa64abb2c4ef95d7542de6096707cd94
ratihfriska <ratihfriska@users.noreply.github.com>
2021-10-11 16:53:05 +0700

    PBO 04

---
+++ PBO 4/ProjectOverloading/src/projectoverloading/ProjectOverloading.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectoverloading;
+
+/**
+ *
+ * @author hp
+ */
+public class ProjectOverloading {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyClass m = new MyClass();
+        
+        m.myMethod();
+        m.myMethod(1.20);
+        m.myMethod(2, 2.0);
+        
+    }
+
+}
+
